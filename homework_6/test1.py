5f83fde1dcf2ecc1c9d00a6842174d0f5034721a
Tema Vasilev <brylev.15@mail.ru>
2024-05-05 17:40:32 +0300

    fix

--- homework_6/test1.py
+++ homework_6/test1.py
@@ -1,1 +1,33 @@
-from for_test import average_numdef test_average_num_valid_input():    assert average_num([1, 2, 3, 4, 5]) == 3.0def test_average_num_mixed_input():    assert average_num([1, 2.5, 3, 4, 5.5]) == 3.0def test_average_num_empty_list():    assert average_num([]) == 0.0def test_average_num_negative_numbers():    assert average_num([-1, -2, -3, -4, -5]) == -3.0def test_average_num_float_input():    assert average_num([1.5, 2.5, 3.5, 4.5, 5.5]) == 3.5def test_average_num_string_input():    assert average_num(['1', '2', '3', '4', '5']) == 3.0def test_average_num_mixed_string_input():    assert average_num(['1', 2, '3', 4, '5']) == 3.0def test_average_num_invalid_input():    assert average_num(['a', 'b', 'c']) == "Bad request"
+from for_test import average_num
+
+
+def test_average_num_valid_input():
+    assert average_num([1, 2, 3, 4, 5]) == 3.0
+
+
+def test_average_num_mixed_input():
+    assert average_num([1, 2.5, 3, 4, 5.5]) == 3.0
+
+
+def test_average_num_empty_list():
+    assert average_num([]) == 0.0
+
+
+def test_average_num_negative_numbers():
+    assert average_num([-1, -2, -3, -4, -5]) == -3.0
+
+
+def test_average_num_float_input():
+    assert average_num([1.5, 2.5, 3.5, 4.5, 5.5]) == 3.5
+
+
+def test_average_num_string_input():
+    assert average_num(['1', '2', '3', '4', '5']) == 3.0
+
+
+def test_average_num_mixed_string_input():
+    assert average_num(['1', 2, '3', 4, '5']) == 3.0
+
+
+def test_average_num_invalid_input():
+    assert average_num(['a', 'b', 'c']) == "Bad request"
